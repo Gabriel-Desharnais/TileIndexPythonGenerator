87a3fde4d8ff467d6b8f29a7d127c0fed94248e0
Gabriel Desharnais <gabriel.desharnais@hotmail.com>
2018-07-26 18:04:14 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import mapycli
+setup(
+    name='mapycli',
+    version='0.0.0.3',
+    description='Python module to generate a TileIndex',
+    url='https://github.com/Gabriel-Desharnais/TileIndexPythonGenerator',
+    author='Gabriel Desharnais',
+    author_email='gabriel.desharnais@hotmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+        ],
+    keywords='mapserver mapfile tileindex',
+    packages=find_packages(),
+
+    install_requires=['ogc',],
+
+
+    )
